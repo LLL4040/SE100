18fa0946586c11309c14faeb6f41784614c7e23d
LLL4040 <2293114926@qq.com>
2019-04-14 10:46:14 +0800

    add spring actuator and security

--- RestfulWordladder/src/main/java/wordladder/GreetingController.java
+++ RestfulWordladder/src/main/java/wordladder/GreetingController.java
@@ -2,9 +2,8 @@ package wordladder;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 @Controller
 public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
